04bb62e854540fe241d9f0c5b26ba49ff1ec5d6c
Harry Thompson <Harry-Thompson@users.noreply.github.com>
2016-04-04 19:54:40 +0100

    Update boot.s

--- Kernel/boot/boot.s
+++ Kernel/boot/boot.s
@@ -1,11 +1,7 @@
 .set ALIGN,    1<<0
-
 .set MEMINFO,  1<<1
-
 .set FLAGS,    ALIGN | MEMINFO
-
 .set MAGIC,    0x1BADB002 
-
 .set CHECKSUM, -(MAGIC + FLAGS)
 
 .section .multiboot
@@ -15,23 +11,19 @@
 .long CHECKSUM
 
 .section .bootstrap_stack, "aw", @nobits
-
-stack_bott
-
-om:
+stack_bottom:
 .skip 16384 # 16 KiB
 stack_top:
 
 .section .text
 .global _start
 .type _start, @function
-_start:
-
-movl $stack_top, %esp
-
-call kernel_main
-
-cli
+_start::
+	movl $stack_top, %esp
+	
+	call main
+	
+	cli
 	hlt
 .Lhang:
 	jmp .Lhang
